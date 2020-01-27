ae6c98e9369267a023b4af687bde33c99c2c131c
Roman Triputko <yeahroma@gmail.com>
2020-01-27 10:21:46 +0300

    feat: apply webpack

---
+++ piskel-app/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        app: ['@babel/polyfill', './src/index.js']
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader"
+            ]
+        },
+        {
+            test: /\.(png|jpg|svg)/,
+            use: 'url-loader'
+        },
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/'
+        },
+        {
+            test: /\.(png|jpg|gif|svg)$/,
+            use: [
+              {
+                loader: 'file-loader',
+              },
+            ],
+        },
+    ]
+    },
+    devServer: {
+        overlay: true
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })
+    ]
+}
