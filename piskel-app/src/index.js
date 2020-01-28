33a307f9862b5b91c095d42b6b9afc77054cbaa7
Roman Triputko <yeahroma@gmail.com>
2020-01-28 16:58:59 +0300

    feat: create entry point

---
+++ piskel-app/src/index.js
@@ -0,0 +1,9 @@
+import './screens/canvas/style.css';
+import './screens/preview/style.css';
+import './components/tools/style.css';
+import './components/canvas-size-buttons/style.css';
+import './components/canvas-color-picker/style.css';
+import "./components/frames-list/style.css";
+import { startApp } from './app';
+
+startApp();
