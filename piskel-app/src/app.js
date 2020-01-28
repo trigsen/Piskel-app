54dfe0a16601a7be8311d9e977c87a8df8ea7577
Roman Triputko <yeahroma@gmail.com>
2020-01-28 16:56:37 +0300

    feat: create entry point

---
+++ piskel-app/src/app.js
@@ -0,0 +1,91 @@
+import Pen from './components/tools/pen/pen'
+import {changeCanvasSize} from './components/canvas-size-buttons/canvas-size-buttons'
+import ColorChangerPanel from './components/canvas-color-picker/canvas-color-picker';
+import ColorBucket from './components/tools/color-bucket/color-bucket';
+import Frames from './components/frames-list/frames';
+import Preview from './screens/preview/preview';
+
+export const startApp = function startApp() {
+    let canvas = document.querySelector('.main-canvas');
+    let context = canvas.getContext('2d');
+    const color = document.querySelector('.color-picker');
+    const framesArray = [];
+    framesArray.push(document.querySelector('.frame'));
+    context.clearRect(0, 0, 700, 600);
+    const eraserColor = {
+        value: '#FFFFFF'
+    };
+    canvas.width = 512;
+    canvas.height = 512;
+    Preview.displayAnimation(framesArray);
+    
+    function deleteActiveTool() {
+        document.querySelector('.active-tool').classList.remove('active-tool');
+    }
+
+    function addActiveTool(target) {
+        target.classList.add('active-tool');
+    }
+
+    document.querySelector('.canvas-size-button').addEventListener('click', (event) => {
+        if (event.target.classList.contains('canvas-size-button__item')) {
+            changeCanvasSize(event.target, canvas);
+        }
+    });
+
+    document.querySelector('.pen').addEventListener('click', (event) => {
+        deleteActiveTool();
+        addActiveTool(event.target);
+        Pen.draw(context, canvas, color, event.target);
+    });
+
+    document.querySelector('.eraser').addEventListener('click', (event) => {
+        deleteActiveTool();
+        addActiveTool(event.target);
+        Pen.draw(context, canvas, eraserColor, event.target);
+    });
+
+    document.querySelector('.color-picker').addEventListener('input', (event) => {
+        ColorChangerPanel.chooseNewColor(event.target.value, color);
+    });
+
+    document.querySelector('.prev-color').addEventListener('click', () => {
+        ColorChangerPanel.choosePrevColor(color);
+    });
+
+    document.querySelector('.paint-bucket').addEventListener('click', (event) => {
+        deleteActiveTool();
+        addActiveTool(event.target);
+        ColorBucket.flood(context, canvas, color, event.target);
+    });
+
+    document.querySelector('.frame-button__text').addEventListener('click', () => {
+        Frames.createNewFrame(canvas, context, framesArray);
+    });
+
+    document.querySelector('.frames__preview').addEventListener('click', (event) => {
+        function getCanvas() {
+            let canvas;
+            event.target.parentNode.childNodes.forEach((child) => {
+                if (child.tagName === 'CANVAS') {
+                canvas = child;   
+            }
+            });
+            return canvas;
+        }
+        
+        if (event.target.classList.contains('frame')) {
+            Frames.chooseFrame(canvas, context, event.target);
+        }
+
+        if (event.target.classList.contains('copy-icon')) {
+            const canvasToCopy = getCanvas();
+            Frames.copyFrame(canvas, context, canvasToCopy, framesArray);
+        }
+
+        if (event.target.classList.contains('delete-icon')) {
+            const canvasToDelete = getCanvas();
+            Frames.deleteFrame(canvas, context, framesArray, canvasToDelete);
+        }
+    });
+}
