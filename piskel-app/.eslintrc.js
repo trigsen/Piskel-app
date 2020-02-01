9cee09e34a76fafd68ce3dc9ca03ae0f12cedcb8
Roman Triputko <yeahroma@gmail.com>
2020-02-01 19:37:27 +0300

    feat: change eslint config

--- piskel-app/.eslintrc.js
+++ piskel-app/.eslintrc.js
@@ -14,7 +14,11 @@ module.exports = {
     "rules": {
         "no-empty": ["error", { "allowEmptyCatch": true }],
         "class-methods-use-this": ["error", { "exceptMethods": ["initialization"] }],
-        "no-undef": 0
+        "no-undef": 0,
+        "import/prefer-default-export": "off",
+        "no-param-reassign": [2, { "props": false }],
+        "no-restricted-syntax": ["error", "BinaryExpression[operator='in']"],
+        "no-plusplus": "off"
     },
 	"parserOptions": {
     "ecmaVersion": 2017,
